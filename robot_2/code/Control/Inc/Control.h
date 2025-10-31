eeb23bf8f9e7b8c21a70ba59d303fb03bee5380d
Shan7 <wei.ky@outlook.com>
2025-10-31 13:55:42 +0800

    控制算法修改

---
+++ robot_2/code/Control/Inc/Control.h
@@ -0,0 +1,14 @@
+//
+// Created by Shan7 on 2025/10/21.
+//
+
+#ifndef CODE_CONTROL_H
+#define CODE_CONTROL_H
+
+void Control(); // 主控制函数，将其他函数放入其中调用
+
+void Movement(); // 移动
+
+void LiftUp();  // 抬起外货舱
+
+#endif //CODE_CONTROL_H
